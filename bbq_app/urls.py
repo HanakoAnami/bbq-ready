5db397d4ce252d0ab4f4b18a2b0eaae944f556a1
穴見英子 <anamihanako@AnamiHanako-MacBook-Air.local>
2026-02-19 16:52:05 +0900

    ログアウト実装

--- bbq_app/urls.py
+++ bbq_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     path('home/', views.home, name='home'),
-    
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
